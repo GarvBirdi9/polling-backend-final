13a6c9b42c93ea3a1f19e4947136cbb585ab7db6
alphanumeric100 <sensiblewhiplash@gmail.com>
2024-08-01 01:33:00 +0530

    first commit

---
+++ modules/controllers/question-controller.js
@@ -0,0 +1,42 @@
+// route ke callback function ismein jaate hain
+// yahan par model ka use hoga
+
+import { ProductModel } from "../models/question-schema.js";
+
+const showAllQuestions = async (req, res) => {
+  const data = await ProductModel.find({});
+
+  res.send(data);
+};
+
+const getAQuestion = async (req, res) => {
+  const { question } = req.body;
+  const data = await ProductModel.find({ question });
+
+  res.json(data);
+};
+
+const makeAQuestion = async (req, res) => {
+  const { question, options, password } = req.body;
+  const data = await ProductModel.create({ question, options, password });
+  res.json({ message: "question has been created" });
+};
+
+const enterAQuestion = async (req, res) => {
+  const { question, password } = req.body;
+
+  const data = await ProductModel.find({ question, password });
+  res.json(data);
+};
+
+const updateAPoll = async (req, res) => {
+  const { question, option } = req.body;
+  const newPolledEntry = { optionSelected: option };
+  const data = await ProductModel.findOneAndUpdate(
+    { question },
+    { $push: { polled: newPolledEntry } }
+  );
+  res.json({ message: "You have submited your poll" });
+};
+
+export { showAllQuestions, makeAQuestion, enterAQuestion, updateAPoll ,getAQuestion };
